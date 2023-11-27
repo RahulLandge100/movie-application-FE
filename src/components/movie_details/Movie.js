cde067b0cc2b76413f5636bee3bdbecb5721d751
Rahul <you@example.com>
2023-11-28 00:01:57 +0530

    Front end for Movie Application

---
+++ src/components/movie_details/Movie.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useCallback } from "react";
+import Card from "../UI/Card";
+import MovieDashboard from './MovieDashboard';
+
+const Movie = () => {
+    const [MovieDetails, setMovieDetails] = useState([]);
+
+    const fetchMovieDetails = useCallback(async () => {
+        const BASE_URL = "http://localhost:8080/movie/get-All-Movies";
+    
+            try {
+            const response = await fetch(BASE_URL);
+            const data = await response.json();
+            let loadedData = [];
+            loadedData = Object.values(data);
+            setMovieDetails(loadedData);
+            } catch (error) {
+            console.log(error);
+            }
+        }, []);
+
+        useEffect(() => {
+            fetchMovieDetails();
+        },[]);
+  
+      return (
+      <>
+      <MovieDashboard MovieDetails={MovieDetails}/>
+      </>
+    );
+  };
+  
+  export default Movie;
